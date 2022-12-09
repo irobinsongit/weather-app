83a8b50209fbf650ddea7d4d2a1fef870b5c3b6d
irobinsongit <117827669+irobinsongit@users.noreply.github.com>
2022-12-09 14:47:37 +0000

    Check desc
    Broke and fixed the entirety of the forecast element, followed along and had to remove and re-add everything from scratch. Icons, temperatures (low and high) days are now returning correctly.

--- src/javascript.js
+++ src/javascript.js
@@ -22,30 +22,58 @@ let dayNow = days[now.getDay()];
 dayInner.innerHTML = `| ${dayNow}`;
 timeInner.innerHTML = `${timeNow}`;
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["mon", "tues", "wed", "thur", "fri", "sat", "sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-            <div class="col-2">
-              <div class="weather-forecast-day">${day}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+            <div class="col-1">
+              <div class="weather-forecast-day">${formatDay(
+                forecastDay.dt
+              )}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="33"
                 />
                 <div class="weather-forecast-temps">
-                  <span class="weather-forecast-max">18°</span>
-                  <span class="weather-forecast-min">12°</span>
+                  <span class="weather-forecast-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
             </div>
         `;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -65,6 +93,7 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function search(city) {
